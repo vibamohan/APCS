b35479f8fe12a48db3872169ee49a6d335652354
Viba Mohan <happymorning552@gmail.com>
2025-10-17 08:39:55 -0700

    warmup
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>

--- QuarterProject/Q1/src/Runner.java
+++ QuarterProject/Q1/src/Runner.java
@@ -1,11 +1,28 @@
-import javax.swing.JFrame;
+import java.util.*;
+import javax.swing.*;
 
 public class Runner {
   public static void main(String[] args) {
-    JFrame frame = new JFrame("Simple Eiffel Tower");
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter 1 for day or 2 for night:");
+    int dayChoice = sc.nextInt();
+    boolean isDay = (dayChoice == 1);
+
+    System.out.println("Enter weather: 1 for sunny, 2 for cloudy, 3 for rainy:");
+    int weatherChoice = sc.nextInt();
+    String weather =
+        switch (weatherChoice) {
+          case 1 -> "sunny";
+          case 2 -> "cloudy";
+          case 3 -> "rainy";
+          default -> "sunny";
+        };
+
+    JFrame frame = new JFrame("Eiffel Tower Scenery");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setResizable(true);
-    frame.add(new Scenery()); // Add the scenery panel
+    frame.add(new Scenery(isDay, weather));
+    frame.pack();
     frame.setVisible(true);
   }
 }
--- QuarterProject/Q1/src/Scenery.java
+++ QuarterProject/Q1/src/Scenery.java
@@ -1,131 +1,191 @@
 import java.awt.*;
 import javax.swing.*;
 
-enum Weather {
-  SUNNY,
-  RAINY,
-  CLOUDY
-}
-
-enum Colors {
-  RED(255, 0, 0);
-
-  int r, g, b;
-  Color c;
-
-  Colors(int r, g, b) {
-    c = new Color(r, g, b);
-  }
-
-  public Color get() {
-    return c;
-  }
-}
-
 public class Scenery extends JPanel {
-  private boolean dayOrNight;
-  private Weather weather;
+  private boolean isDay;
+  private String weather;
 
-  public Scenery() {
-    this.dayOrNight = true;
-    this.weather = Weather.SUNNY;
-    this.colors = Color.CYAN;
-  }
 
-  public Scenery(boolean dayOrNight, Weather weather) {
-    this.dayOrNight = dayOrNight;
+  public Scenery(boolean isDay, String weather) {
+    this.isDay = isDay;
     this.weather = weather;
-    this.colors = dayOrNight ? Color.CYAN : Color.DARK_GRAY;
   }
 
-  @Override
+
   public Dimension getPreferredSize() {
-    return new Dimension(1300, 800);
+    return new Dimension(1000, 700);
+  }
+
+
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    drawBackground(g);
+    drawGround(g);
+
+    drawEiffelTower(g);
+
+    drawTrees(g);
+    drawBenches(g);
+    drawFlowers(g);
+    drawLampPosts(g);
   }
 
   @Override
-  protected void paintComponent(Graphics g) {
-    super.paintComponent(g);
+  public Color getBackground() {
+    return (isDay) ? new Color(135, 206, 250) : new Color(25, 25, 112);
+  }
 
-    g.setColor(dayOrNight ? Color.CYAN : Color.BLACK);
-    g.fillRect(0, 0, getWidth(), getHeight());
 
-    switch (weather) {
-      case SUNNY -> g.setColor(Color.YELLOW);
-      case RAINY -> g.setColor(Color.BLUE);
-      case CLOUDY -> g.setColor(Color.LIGHT_GRAY);
+  private void drawBackground(Graphics g) {
+    if (weather.equals("sunny") && isDay) drawSun(g);
+    if (weather.equals("cloudy") || weather.equals("rainy")) drawClouds(g);
+    if (weather.equals("rainy")) drawRain(g);
+  }
+
+
+  private void drawGround(Graphics g) {
+    if (isDay) g.setColor(new Color(34, 139, 34));
+    else g.setColor(new Color(0, 70, 0));
+    g.fillRect(0, getHeight() - 100, getWidth(), 100);
+  }
+
+
+  private void drawEiffelTower(Graphics g) {
+    Graphics2D g2d = (Graphics2D) g;
+    g2d.setColor(Color.ORANGE);
+    g2d.setStroke(new BasicStroke(10.0f));
+
+    int xOffset = 220;
+    int yOffset = 30;
+
+    g2d.fillPolygon(
+        new int[] {100 + xOffset, 200 + xOffset, 300 + xOffset},
+        new int[] {
+          getHeight() - 200 - yOffset, getHeight() - 500 - yOffset, getHeight() - 200 - yOffset
+        },
+        3);
+
+    g2d.fillPolygon(
+        new int[] {80 + xOffset, 100 + xOffset, 330 + xOffset, 250 + xOffset},
+        new int[] {
+          getHeight() - 170 - yOffset,
+          getHeight() - 190 - yOffset,
+          getHeight() - 180 - yOffset,
+          getHeight() - 180 - yOffset
+        },
+        4);
+
+    g2d.fillPolygon(
+        new int[] {100 + xOffset, 330 + xOffset, 330 + xOffset, 100 + xOffset},
+        new int[] {
+          getHeight() - 190 - yOffset,
+          getHeight() - 190 - yOffset,
+          getHeight() - 170 - yOffset,
+          getHeight() - 170 - yOffset
+        },
+        4);
+
+    g2d.fillPolygon(
+        new int[] {60 + xOffset, 100 + xOffset, 330 + xOffset, 370 + xOffset},
+        new int[] {
+          getHeight() - 70 - yOffset,
+          getHeight() - 160 - yOffset,
+          getHeight() - 160 - yOffset,
+          getHeight() - 70 - yOffset
+        },
+        4);
+
+    g2d.setColor(getBackground());
+    g2d.fillPolygon(
+        new int[] {170 + xOffset, 200 + xOffset, 230 + xOffset},
+        new int[] {
+          getHeight() - 300 - yOffset, getHeight() - 400 - yOffset, getHeight() - 300 - yOffset
+        },
+        3);
+
+    g2d.fillArc(100 + xOffset, getHeight() - 135 - yOffset, 200, 130, 0, 180);
+  }
+
+  private void drawTrees(Graphics g) {
+    int yOff = 60;
+    drawTree(g, 120, 480 + yOff);
+    drawTree(g, 230, 500 + yOff);
+    drawTree(g, 800, 490 + yOff);
+    drawTree(g, 900, 470 + yOff);
+  }
+
+  private void drawTree(Graphics g, int x, int y) {
+    g.setColor(new Color(101, 67, 33)); 
+    g.fillRect(x + 10, y, 20, 60);
+    g.setColor(Color.GREEN); 
+    g.fillOval(x - 10, y - 40, 60, 50);
+  }
+
+  private void drawBenches(Graphics g) {
+    drawBench(g, 300, 580);
+    drawBench(g, 700, 580);
+  }
+
+  private void drawBench(Graphics g, int x, int y) {
+    g.setColor(new Color(139, 69, 19));
+    g.fillRect(x, y, 60, 10);
+    g.fillRect(x, y + 15, 60, 10);
+    g.fillRect(x + 5, y + 25, 5, 20);
+    g.fillRect(x + 50, y + 25, 5, 20);
+  }
+
+  private void drawFlowers(Graphics g) {
+    for (int i = 0; i < 12; i++) {
+      drawFlower(g, 40 + i * 80, getHeight() - 50);
     }
-
-    // Draw scenery
-    eiffelTower(g, 400, 100, 120, 600);
-    drawTree(g, 100, 700);
-    drawBakery(g, 800, 700);
-    drawCat(g, 600, 730);
-    drawFlower(g, 1000, 750);
   }
 
-  public void eiffelTower(Graphics g, int x, int y, int width, int height) {
-    g.setColor(Color.ORANGE);
+  private void drawFlower(Graphics g, int x, int y) {
+    g.setColor(new Color(0, 128, 0));
+    g.fillRect(x + 6, y, 4, 25);
 
-    int legHeight = height * 2 / 3; // height of the bottom legs
-    int platformHeight = y + legHeight; // middle platform
-    int legInset = width / 5; // how much legs taper inward
-
-    // ---- Draw bottom legs ----
-    g.fillPolygon(
-        new int[] { x, x + width, x + width - legInset, x + legInset },
-        new int[] { y + height, y + height, platformHeight, platformHeight },
-        4);
-
-    // Draw middle platform
-    g.fillRect(x + legInset, platformHeight - 10, width - 2 * legInset, 10);
-
-    // ---- Draw top A-shaped section ----
-    int topHeight = height / 3;
-    int topWidth = width / 2;
-    int topX = x + width / 2 - topWidth / 2;
-    int topY = y;
-    g.fillPolygon(
-        new int[] { topX, topX + topWidth, x + width - legInset, x + legInset },
-        new int[] { topY + topHeight, topY + topHeight, platformHeight, platformHeight },
-        4);
-
-    // ---- Draw antenna ----
-    g.fillRect(x + width / 2 - 2, y - 20, 4, 20);
-  }
-
-  public void drawTree(Graphics g, int x, int y) {
-    g.setColor(new Color(101, 67, 33));
-    g.fillRect(x, y - 50, 20, 50);
-    g.setColor(Color.GREEN);
-    g.fillOval(x - 15, y - 80, 50, 40);
-  }
-
-  public void drawBakery(Graphics g, int x, int y) {
-    g.setColor(new Color(255, 200, 150));
-    g.fillRect(x, y - 50, 60, 50);
-    g.setColor(Color.RED);
-    g.fillRect(x + 10, y - 30, 40, 20);
-  }
-
-  public void drawCat(Graphics g, int x, int y) {
-    g.setColor(Color.GRAY);
-    g.fillOval(x, y, 20, 20);
-    g.fillOval(x + 15, y - 10, 10, 10);
-  }
-
-  public void drawFlower(Graphics g, int x, int y) {
-    g.setColor(Color.GREEN);
-    g.fillRect(x, y, 5, 20);
     g.setColor(Color.PINK);
     g.fillOval(x - 5, y - 10, 15, 15);
+    g.fillOval(x + 5, y, 15, 15);
+    g.fillOval(x - 15, y, 15, 15);
+    g.fillOval(x - 5, y + 10, 15, 15);
+
+    g.setColor(Color.YELLOW);
+    g.fillOval(x, y + 5, 10, 10);
   }
 
-  public static void main(String[] args) {
-    JFrame frame = new JFrame("Scenery Demo");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.add(new Scenery(true, Weather.SUNNY));
-    frame.pack();
-    frame.setVisible(true);
+  private void drawLampPosts(Graphics g) {
+    drawLampPost(g, 180, 600);
+    drawLampPost(g, 860, 600);
+  }
+
+  private void drawLampPost(Graphics g, int x, int y) {
+    g.setColor(Color.DARK_GRAY);
+    g.fillRect(x, y - 80, 10, 80);
+    g.setColor(Color.YELLOW);
+    g.fillOval(x - 5, y - 90, 20, 20);
+  }
+
+  private void drawSun(Graphics g) {
+    g.setColor(Color.YELLOW);
+    g.fillOval(800, 100, 80, 80);
+  }
+
+  private void drawClouds(Graphics g) {
+    g.setColor(Color.LIGHT_GRAY);
+    for (int i = 0; i < 3; i++) {
+      g.fillOval(200 + i * 200, 100, 100, 60);
+      g.fillOval(240 + i * 200, 80, 100, 60);
+    }
+  }
+
+  private void drawRain(Graphics g) {
+    g.setColor(Color.BLUE);
+    for (int i = 0; i < getWidth(); i += 15) {
+      for (int j = 0; j < 4; j++) {
+        int y = (int) (Math.random() * 400 + 200);
+        g.drawLine(i, y, i + 2, y + 10);
+      }
+    }
   }
 }
